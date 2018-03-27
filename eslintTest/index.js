72bb69bf19ede68064fe89042c326f1f71945cce
iosky <2061469002@qq.com>
2018-03-27 16:36:14 +0800

    学习使用es6的module

--- eslintTest/index.js
+++ eslintTest/index.js
@@ -1,3 +1,14 @@
 function hello() {
   console.log('hello world');
 }
+
+window.onload = () => {
+  import { plus } from "./test";
+  import person from './test';
+  import {firstName, lastName, year} from './test.js';
+  hello();
+  console.log(firstName , lastName);
+  function setName(element) {
+    element.textContent = firstName + ' ' + lastName;
+  }
+}
---
+++ eslintTest/test.js
@@ -0,0 +1,19 @@
+export let firstName = 'Michael';
+export let lastName = 'Jackson';
+export let year = 1958;
+export function multiply(x, y) {
+  return x * y;
+}
+
+export function plus(x, y) {
+  return x + y;
+}
+const name = 'Tom';
+const sex = 'man';
+const age = '20';
+
+export default {
+  name,
+  sex,
+  age
+}
